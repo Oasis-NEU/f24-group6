d98f6731c9f8b3cc6688b9ebf2ba2efc6bb57e49
yurikakan <107583678+yurikakan@users.noreply.github.com>
2024-09-29 12:52:38 -0400

    new file in src/component

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+//importing function from components
+import Welcome from './components/welcome'
+
+function App() {
+
+  // if many - then use parenthesis
+  return (
+    <Welcome />
+    // another function
+  );
+}
+
+export default App
---
+++ my-app/src/components/welcome.jsx
@@ -0,0 +1,8 @@
+// export default - make it available to other components 
+export default function Welcome() {
+    return (
+        <h1>
+            Welcome to Feline Focus!
+        </h1>
+    );
+}
