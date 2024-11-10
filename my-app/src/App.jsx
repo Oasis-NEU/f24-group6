08a02c1477f02a62584d77f6210427290134082c
yurikakan <107583678+yurikakan@users.noreply.github.com>
2024-11-10 14:00:23 -0500

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 //importing function from components
-import Welcome from './components/welcome'
 import Header from './components/productivity/todolist/header'
 import TodoList from './components/productivity/todolist'
 
